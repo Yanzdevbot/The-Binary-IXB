46b649d1b41c2a3488e1fe3589a35c99b48544b1
Raol Mukarrozi <vgxurl@gmail.com>
2025-07-29 18:30:28 +0800

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
-import React, { useEffect } from "react"
+"use client"
+
+import { useEffect } from "react"
 import Home from "./Pages/Home"
 import Carousel from "./Pages/Gallery"
 import FullWidthTabs from "./Pages/Tabs"
@@ -8,32 +10,32 @@ import AOS from "aos"
 import "aos/dist/aos.css"
 
 function App() {
-	useEffect(() => {
-		AOS.init()
-		AOS.refresh()
-	}, [])
+  useEffect(() => {
+    AOS.init()
+    AOS.refresh()
+  }, [])
 
-	return (
-		<>
-			<Home />
+  return (
+    <>
+      <Home />
 
-			<Carousel />
-			<FullWidthTabs />
+      <Carousel />
+      <FullWidthTabs />
 
-			<div id="Mesh1"></div>
+      <div id="Mesh1"></div>
 
+      <div
+        className="lg:mx-[12%] lg:mt-[-5rem] lg:mb-20 hidden lg:block"
+        id="ChatAnonim_lg"
+        data-aos="fade-up"
+        data-aos-duration="1200"
+      >
+        <Chat />
+      </div>
 
-			<div
-				className="lg:mx-[12%] lg:mt-[-5rem] lg:mb-20 hidden lg:block"
-				id="ChatAnonim_lg"
-				data-aos="fade-up"
-				data-aos-duration="1200">
-				<Chat />
-			</div>
-
-			<Footer />
-		</>
-	)
+      <Footer />
+    </>
+  )
 }
 
 export default App
