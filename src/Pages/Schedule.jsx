a000e9079cf5edd1d6f1b094a7326561e52af5f8
Raol Mukarrozi <vgxurl@gmail.com>
2025-07-31 06:07:36 +0800

    Add files via upload

--- src/Pages/Schedule.jsx
+++ src/Pages/Schedule.jsx
@@ -12,23 +12,53 @@ const Jumat = React.lazy(() => import("../components/Mapel/Jumat"))
 
 const Schedule = () => {
   const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-  const currentDay = daysOfWeek[new Date().getDay()]
+  const currentDayIndex = new Date().getDay()
+  const currentDay = daysOfWeek[currentDayIndex]
 
   useEffect(() => {
     AOS.init()
     AOS.refresh()
   }, [])
 
-  const piketGroup = []
+  const piketGroup = [
+    [],
+    [
+      "Aida Nur Fadhillah",
+      "NOR AJKIA",
+      "MUHAMMAD ALVIE",
+      "Ferdy Samantha Situmorang",
+      "Hafizi",
+      "Muhammad Rendy Saputra",
+      "Hanifah Sekar Kinarsih Imandariani",
+    ],
+    ["Apriani", "FAHRENDY RIFQI PRATAMA", "GISELA NETANYA", "Naila Amuna", "Raol Mukarrozi", "Rafa Akbar"],
+    ["Dewi Nasya Ramadhani", "Nabila Assyifa", "NUR ANITA", "Rizwan Chandra Ramadhani", "Ropikoh", "Muhammad Zidan"],
+    [
+      "AYDIN JAVAS NARARYA",
+      "NUR SALSABILLA",
+      "Muhammad Dhafin Razata",
+      "Ikhlas Cassanova Darmawan",
+      "Jesica Adelia Agustin",
+      "Gusti Selvia Yulinda",
+      "Wulandari",
+    ],
+    [
+      "Amirotul Muslimah Putriani",
+      "SRI RAHAYU",
+      "Kulfan Rifki Sampurna",
+      "NAZARENA JASMINE QORI",
+      "MUHAMMAD RAFFA ARSYAFANI",
+      "Sonya Jun Maxima Lau",
+    ],
+    [],
+  ]
+
+  const currentPiketNames = piketGroup[currentDayIndex]
 
   const dayComponents = [null, Senin, Selasa, Rabu, Kamis, Jumat]
-
-  const TodayComponent = dayComponents[new Date().getDay()]
-
-  const currentPiketNames = piketGroup[new Date().getDay() - 1]
+  const TodaySubjectComponent = dayComponents[currentDayIndex]
 
   console.log("Current Day:", currentDay)
-  console.log("Piket Group:", piketGroup)
   console.log("Current Piket Names:", currentPiketNames)
 
   return (
@@ -36,15 +66,15 @@ const Schedule = () => {
       <div className="lg:flex lg:justify-center lg:gap-32 lg:mb-10 lg:mt-16 ">
         <div className="text-white flex flex-col justify-center items-center mt-8 md:mt-3 overflow-y-hidden">
           <div className="text-2xl font-medium mb-5" data-aos="fade-up" data-aos-duration="500">
-            {currentDay}
+            Jadwal Mata Pelajaran {currentDay}
           </div>
           <div data-aos="fade-up" data-aos-duration="400">
-            {TodayComponent ? (
+            {TodaySubjectComponent ? (
               <React.Suspense fallback={<p>Loading...</p>}>
-                <TodayComponent />
+                <TodaySubjectComponent />
               </React.Suspense>
             ) : (
-              <p className="opacity-50">Tidak Ada Jadwal Hari Ini</p>
+              <p className="opacity-50">Tidak Ada Jadwal Mata Pelajaran Hari Ini</p>
             )}
           </div>
         </div>
@@ -52,7 +82,7 @@ const Schedule = () => {
 
       <div className="text-white flex flex-col justify-center items-center mt-8 lg:mt-0 lg:mb-[10rem] mb-10 overflow-y-hidden">
         <div className="text-2xl font-medium mb-5 text-center" data-aos="fade-up" data-aos-duration="500">
-          Piket
+          Piket {currentDay}
         </div>
         {currentPiketNames && currentPiketNames.length > 0 ? (
           currentPiketNames.map((piketName, index) => (
@@ -68,7 +98,7 @@ const Schedule = () => {
             </div>
           ))
         ) : (
-          <p className="opacity-50">Tidak Ada Jadwal Hari Ini</p>
+          <p className="opacity-50">Tidak Ada Jadwal Piket Hari Ini</p>
         )}
       </div>
     </>
